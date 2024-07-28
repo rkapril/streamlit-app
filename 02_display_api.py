5f7cd11785bd4c67523adf2d50cee011e83f8b83
rkapril <rkapril@me.com>
2024-07-29 00:01:45 +0800

    updated codebase

--- 02_display_api.py
+++ 02_display_api.py
@@ -5,3 +5,17 @@ import numpy as np
 # display almost anything
 # st.write
 st.write("Hello World")
+
+st.write("Welcome to Streamlit App APIs")
+
+st.write(1234)
+
+df = pd.DataFrame({
+    'first_column': [1, 2, 3, 4],
+    'second_column': [10, 20, 30, 40]
+})
+
+st.write(df)
+
+# display numpy array
+st.write(np.array([1, 2, 3, 4]))
